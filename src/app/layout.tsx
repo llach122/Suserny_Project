a39b35c7e9d37d7aa82020f0459f3b4ef041b306
Samm133 <alvaro.alvillar@gmail.com>
2025-03-20 16:15:43 -0300

    Correcion en layout y globals.css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 // app/layout.tsx
 import './globals.css';
 import type { Metadata } from 'next';
+import Footer from './components/Footer';
+import Contact from './components/Contact';
+import About from './components/About';
+import WhatsAppBubble from './components/WhatsAppBubble';
+
 
 export const metadata: Metadata = {
   title: 'REFLEJO - Espejos de Diseño',
@@ -14,7 +19,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
-      <body>{children}</body>
+      <body>
+        {children}
+        <About/>
+        <Contact 
+          phone="+56 **** ****"
+          email="info@reflejo.com"
+          address="Calle Principal 123, Madrid"
+        />
+         <WhatsAppBubble phoneNumber="5612345678" />
+        <Footer company="SUSERNYS" insta="****" twit="***" face="***"/>
+       
+      </body>
+      
     </html>
   );
 }
